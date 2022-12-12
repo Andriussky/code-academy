051f3f85439c6fd44409aeb141090ccd7b35cbfb
Andrius Pranskunas <noriunietis1@gmail.com>
2022-12-12 14:58:51 +0200

    first commit

---
+++ js/naujas.js
@@ -0,0 +1,21 @@
+let rezult = 'Rez.:';
+console.log(rezult)
+let a = 1;
+let b = 'Kintemojo a reiksme: ' + a;
+console.log(b)
+
+let c = 2;
+let d = 'Kintemojo a reiksme: ' + c;
+console.log(d)
+
+let e = 'Andrius Pranskunas';
+let f = 'Mano vardas ';
+let g = f + e;
+console.log(g)
+
+let metai = 1999;
+let menuo = 10;
+let diena = 15;
+let rez = 'rez: ';
+let result = metai % 1990 + menuo % 10 + diena % 10;
+console.log(result)
